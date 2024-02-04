d6193b5dee582256c49a2f5f5a3835df3feaf6cb
ctsdm <156813428+CTSDM@users.noreply.github.com>
2024-02-04 21:04:01 +0100

    Add change color of each box by clicking
    The click can occur anywhere within the document.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 const containerMain = document.querySelector('.container-main');
+let mouse_clicked = false;
 
-let size = 16
+document.addEventListener('mousedown', () => {mouse_clicked = true;}, {capture: true})
+
+document.addEventListener('mouseup', () => {mouse_clicked = false;})
+
+createCanvas(16);
 
 function createCanvas(size) {
     containerMain.replaceChildren();
-    for (let i = 0; i < size; i ++) {
+    for (let i = 0; i < size; i++) {
         const rowContainer = document.createElement('div');
         for (let j = 0; j < size; j++) {
             const rowElement = document.createElement('div');
             rowElement.classList.toggle('squares');
             for (let k = 1; k < size; k++) {
                 rowElement.addEventListener('mouseenter', setRandomColor);
+                rowElement.addEventListener('mousedown', setRandomColor);
             }
             rowContainer.appendChild(rowElement);
         }
@@ -20,8 +26,11 @@ function createCanvas(size) {
 }
 
 function setRandomColor() {
-    let redVal = Math.floor(Math.random() * 256);
-    let greenVal = Math.floor(Math.random() * 256);
-    let blueVal = Math.floor(Math.random() * 256);
-    this.style.backgroundColor = `rgb(${redVal}, ${greenVal}, ${blueVal})`;
+    if (mouse_clicked === true) 
+    {
+        let redVal = Math.floor(Math.random() * 256);
+        let greenVal = Math.floor(Math.random() * 256);
+        let blueVal = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${redVal}, ${greenVal}, ${blueVal})`;
+    }
 }
