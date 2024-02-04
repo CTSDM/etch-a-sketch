71c3dd3090a46f3978c7564139667d6520e15d88
ctsdm <156813428+CTSDM@users.noreply.github.com>
2024-02-04 20:28:55 +0100

    Add random color changes when mouse enters a box

--- js/script.js
+++ js/script.js
@@ -6,12 +6,22 @@ function createCanvas(size) {
     containerMain.replaceChildren();
     for (let i = 0; i < size; i ++) {
         const rowContainer = document.createElement('div');
-        rowContainer.classList.toggle('active');
         for (let j = 0; j < size; j++) {
             const rowElement = document.createElement('div');
             rowElement.classList.toggle('squares');
+            for (let k = 1; k < size; k++) {
+                rowElement.addEventListener('mouseenter', setRandomColor);
+            }
             rowContainer.appendChild(rowElement);
         }
+        rowContainer.classList.toggle('container-boxes');
         containerMain.appendChild(rowContainer);
     }
+}
+
+function setRandomColor() {
+    let redVal = Math.floor(Math.random() * 256);
+    let greenVal = Math.floor(Math.random() * 256);
+    let blueVal = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${redVal}, ${greenVal}, ${blueVal})`;
 }
